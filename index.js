d3157dbe3b38a61dc36a42adae58e5032d3e18aa
Hector <quimat2@hotmail.com>
2023-08-21 09:00:37 -0600

    commit 21/08/2023

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const http = require('http')
-const port = 5000
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
 
-const server = http.createServer((req, res) => {
-    res.end('Hola Alumnos')
+app.use(express.static(__dirname + '/public'))
+
+// Motor de Plantillas o Vistas
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        titulo: 'Index con EJS',
+        parametro: 'Hola'
+    })
 })
 
-server.listen(port, () => [
-    console.log('Arranca el servidor')
+app.get('/lenguajes', (req, res) => {
+    res.send(__dirname + '/public/index.html')
+})
+
+app.listen(port, () => [
+    console.log('Arranca el servidor', port),
+    console.log(`Arranca el servidor ${port}`)
 ])
